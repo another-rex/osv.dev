bd21ae420c0369467cce867af9a2dc539638c563
Rex P <rexpan@google.com>
2025-03-20 13:47:47 +1100

    Add go osvdev package

---
+++ go/internal/testhelper/containserrstr.go
@@ -0,0 +1,22 @@
+package testhelper
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ContainsErrStr is an error that matches other errors that contains
+// `str` in their error string.
+//
+//nolint:errname
+type ContainsErrStr struct {
+	Str string
+}
+
+// Error returns the error string
+func (e ContainsErrStr) Error() string { return fmt.Sprintf("error contains: '%s'", e.Str) }
+
+// Is checks whether the input error contains the string in ContainsErrStr
+func (e ContainsErrStr) Is(err error) bool {
+	return strings.Contains(err.Error(), e.Str)
+}
